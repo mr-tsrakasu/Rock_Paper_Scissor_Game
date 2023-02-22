8d1711e9b3fdace7a16514c2b07cdffea93be42d
Yayra Tsrakasu <mr.tsrakasu@gmail.com>
2023-02-22 10:39:59 +0000

    create variables and a few functions

--- rps_ui.js
+++ rps_ui.js
@@ -1,1 +1,19 @@
-console.log("We are live!!")
+const rock = document.querySelector('.playerSelectionRock')
+const paper = document.querySelector('.playerSelectionPaper')
+const scissor = document.querySelector('.playerSelectionScissor')
+const playerName = ''
+let randomChoice = Math.floor(Math.random() * choices.length)
+let computerChoice = choices[randomChoice]
+let userChoice = ''
+let user = 0
+let agorkoli = 0
+
+function playRound() {
+	
+};
+
+function generateRandomChoice() {
+    randomChoice = Math.floor(Math.random() * choices.length)
+    computerChoice = choices[randomChoice]
+    return computerChoice
+};
